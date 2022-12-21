d70bebca4516c74875a7deec57a127764325ad2d
Conro101 <cbot.596@gmail.com>
2022-12-21 11:34:37 -0500

    Updated front-end URL to controllers

--- src/main/java/com/revature/controllers/AccountController.java
+++ src/main/java/com/revature/controllers/AccountController.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/account")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://angular-app-frontend-sdfgsfdgsfgs.s3-website-us-west-1.amazonaws.com:4200"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000", "http://angular-app-frontend-sdfgsfdgsfgs.s3-website-us-west-1.amazonaws.com"}, allowCredentials = "true")
 public class AccountController {
 
     @Autowired
